3edb4fe05de0893a15e42cb2a0ce9415136461e0
thehanatos <admin4523@gmail.com>
2025-07-16 12:14:08 +0200

    adapt templates and urls for lettings and profiles

--- lettings/views.py
+++ lettings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Letting
 
-# Create your views here.
+
+def lettings_index(request):
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+
+def letting(request, letting_id):
+    letting = get_object_or_404(Letting, id=letting_id)
+    context = {
+        'title': letting.title,
+        'address': letting.address,
+    }
+    return render(request, 'letting.html', context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
 
-# Create your views here.
+
+def index(request):
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+
+def profile(request, username):
+    profile = get_object_or_404(Profile, user__username=username)
+    context = {'profile': profile}
+    return render(request, 'profile.html', context)
