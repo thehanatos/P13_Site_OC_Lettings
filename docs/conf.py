d5d3124f041c428a87bdd1050f8628988de170a5
thehanatos <admin4523@gmail.com>
2025-07-17 16:24:21 +0200

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -30,18 +30,10 @@ html_static_path = ['_static']
 html_logo = "_static/logo.png"
 html_title = "Orange County Lettings"
 
-# html_theme = "sphinx_rtd_theme"
-# html_theme_options = {
-#     "collapse_navigation": False,
-#     "sticky_navigation": True,
-#     "navigation_depth": 3,
-#     "style_external_links": True,
-# }
-
 html_theme = "furo"
 html_theme_options = {
-    "light_logo": "your_logo.png",
-    "dark_logo": "your_logo.png",
+    # "light_logo": "your_logo.png",
+    # "dark_logo": "your_logo.png",
     "sidebar_hide_name": False,
     "navigation_with_keys": True,
 }
